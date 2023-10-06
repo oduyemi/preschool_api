5e4e73eda687d76385f1a943d6406c086f586e63
Yemi Oduyemi <yhermii@gmail.com>
2023-10-06 01:12:23 +0100

    update 2

---
+++ preschool_app/authourize.py
@@ -0,0 +1,35 @@
+# token.py
+
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from instance.config import SECRET_KEY
+
+SECRET_KEY = SECRET_KEY
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_token(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise credentials_exception
--- preschool_app/routes.py
+++ preschool_app/routes.py
@@ -1,21 +1,55 @@
-from fastapi import APIRouter, Request, status
+from datetime import timedelta, datetime
+from fastapi import APIRouter, Request, status, Depends, HTTPException
 from sqlalchemy.orm import Session
 from preschool_app import starter
-from .models import Student
+from .authourize import create_access_token, decode_token, ACCESS_TOKEN_EXPIRE_MINUTES
+from fastapi.security import OAuth2PasswordRequestForm
+from typing import Optional
 
 
 router = APIRouter()
 
-students = []
+def authenticate_user(username: str, password: str) -> Optional[dict]:
 
-@router.get("/")
+    users_db = {
+        "user1": {"username": "user1", "password": "password1"},
+        "user2": {"username": "user2", "password": "password2"},
+    }
+
+    user = users_db.get(username)
+    if user and user["password"] == password:
+        return {"sub": username}
+
+    return None
+
+@starter.get("/")
 async def get_index():
     return{"message": "Welcome to Preschool API"}
 
-@starter.get("/students")
-def get_students():
-    pass 
-    return {"message": "students data"}
+@starter.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": form_data.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@starter.get("/secure-data")
+async def secure_data(current_user: dict = Depends(decode_token)):
+    return {"message": "This is secure data!", "current_user": current_user}
+
+
+
+
+
 
 
 
